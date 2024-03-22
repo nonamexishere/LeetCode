438dcc09551421b39c8742bc06a98a075d5c7bf9
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-03-22 21:45:35 +0300

    Time: 171 ms (58.21%) | Memory: 123.3 MB (39.09%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-       ListNode* tmp = head;
-       int counter = 0;
-       while(tmp) {
-        tmp = tmp->next;
-        counter++;
-       } 
-       tmp = head;
-       vector<int> first_half;
-       int tmp_counter = counter / 2;
-       while (tmp_counter--) {
-        first_half.emplace_back(tmp->val);
-        tmp = tmp->next;
-       }
-       if (counter & 1) tmp = tmp->next;
-        int i = counter / 2;
-       while (tmp) {
-        if(tmp->val != first_half[--i]) return false;
-        tmp = tmp->next;
-       }
-       return true;
+        ListNode* tmp1 = head;
+        ListNode* tmp2 = head;
+        stack<int> s;
+        int counter = 0;
+        while(tmp2) {
+            counter++;
+            tmp2 = tmp2->next;
+            if (counter % 2 == 0) {
+                s.push(tmp1->val);
+                tmp1 = tmp1->next;
+            }
+        } 
+        if (counter & 1) tmp1 = tmp1->next;
+        while(tmp1) {
+            if (tmp1->val == s.top()) s.pop();
+            else return false;
+            tmp1 = tmp1->next;
+        }
+        return true;
     }
 };
