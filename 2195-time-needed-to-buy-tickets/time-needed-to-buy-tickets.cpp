df211713ff5cf5dff324f97a13a11b008119c3ca
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-04-09 17:23:59 +0300

    Time: 0 ms (100.00%) | Memory: 9.3 MB (91.31%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans = 0;
+        for (int i = 0; i <= k; ++i) {
+            if (tickets[i] <= tickets[k]) ans += tickets[i];
+            else ans += tickets[k];
+        }
+        for (int i = k + 1; i < tickets.size(); ++i) {
+            if (tickets[i] < tickets[k]) ans += tickets[i];
+            else ans += tickets[k] - 1;
+        }
+        return ans;
+    }
+};
