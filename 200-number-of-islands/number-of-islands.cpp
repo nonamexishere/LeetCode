33dc2b14923181cb323ee304aab57afe26b29d14
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-04-20 00:12:18 +0300

    Time: 28 ms (62.76%) | Memory: 16.2 MB (54.09%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        stack<pair<int, int>> s;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        int counter = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '0' || visited[i][j]) continue;
+                if (grid[i][j] == '1') {
+                    s.push({i, j});
+                    visited[i][j] = true;
+                    counter++;
+                    while(!s.empty()) {
+                        int tf = s.top().first;
+                        int ts = s.top().second;
+                        s.pop();
+                        if (tf > 0 && !visited[tf - 1][ts] && grid[tf - 1][ts] == '1') {s.push({tf - 1, ts}); visited[tf - 1][ts] = true;}
+                        if (tf < grid.size() - 1 && !visited[tf + 1][ts] && grid[tf + 1][ts] == '1') {s.push({tf + 1, ts}); visited[tf + 1][ts] = true;}
+                        if (ts > 0 && !visited[tf][ts - 1] && grid[tf][ts - 1] == '1') {s.push({tf, ts - 1}); visited[tf][ts - 1] = true;}
+                        if (ts < grid[0].size() - 1 && !visited[tf][ts + 1] && grid[tf][ts + 1] == '1') {s.push({tf, ts + 1}); visited[tf][ts + 1] = true;}
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+};
