234b00c3125473bce335a9c066cba03514576f67
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-04-29 17:42:41 +0300

    Time: 108 ms (64.05%) | Memory: 91.2 MB (85.86%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int p = 0;
         for (int i = 0; i < nums.size(); ++i) {
-            p ^= nums[i];
+            k ^= nums[i];
         }
         int ans = 0;
-        p ^= k;
-        while (p) {
-            ans += (p & 1);
-            p >>= 1;
+        while (k) {
+            ans += (k & 1);
+            k >>= 1;
         }
         return ans;
     }
