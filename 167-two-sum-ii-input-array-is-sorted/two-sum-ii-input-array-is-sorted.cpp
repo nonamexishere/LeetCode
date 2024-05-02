7e424f89ba0fae7bdef3a83a2807c614954c72be
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-02 16:27:43 +0300

    Time: 9 ms (59.19%) | Memory: 17.8 MB (91.38%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int l = 0;
         int r = numbers.size() - 1;
-        int sum = -1001;
+        int sum;
         while ((sum = numbers[l] + numbers[r]) != target) {
             if (sum > target) r--;
             if (sum < target) l++;
         } 
-        vector<int> v;
-        v.emplace_back(l + 1);
-        v.emplace_back(r + 1);
+        vector<int> v(2);
+        v[0] = l + 1;
+        v[1] = r + 1;
         return v;
     }
 };
