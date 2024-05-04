ef959f53a94a48eaa4a96031f200a376229e1a59
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-04 12:07:10 +0300

    Time: 41 ms (97.66%) | Memory: 45.6 MB (8.97%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,3 +1,10 @@
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
@@ -5,17 +12,14 @@ public:
         int l = 0;
         int r = people.size() - 1;
         int ans = 0;
-        while (l <= r) {
-            if (l == r) {
-                ans++;
-                break;
-            }
+        while (l < r) {
             if (people[r] + people[l] <= limit) {
                 l++;
             }
             ans++;
             r--;
         }
+        if (l == r) ans++;
         return ans;
     }
 };
