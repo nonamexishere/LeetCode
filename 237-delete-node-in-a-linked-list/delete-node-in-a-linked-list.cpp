833d11ba845f4a36196fd7e0ebc4f25d55457045
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-06 13:59:02 +0300

    Time: 8 ms (36.04%) | Memory: 11.3 MB (43.50%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -1,17 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-static const int __ = [](){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
 class Solution {
 public:
     void deleteNode(ListNode* node) {
