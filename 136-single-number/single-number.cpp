b710c0f3f2be20c5533b9b0d27097f2ca3da1ff1
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-06 22:34:46 +0300

    Time: 4 ms (97.89%) | Memory: 19.4 MB (31.93%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
         int x = 0;
-        int s = nums.size();
-        for (int i = 0; i < s; ++i) x ^= nums[i];
+        for (int& num : nums) x ^= num;
         return x;
     }
 };
