9464a622f65ffc2c27cfa1364d1c6290fe8a5510
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-07 17:35:55 +0300

    Time: 157 ms (92.91%) | Memory: 120.9 MB (65.02%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -1,11 +1,1 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {public: ListNode* doubleIt(ListNode* head) {ios_base::sync_with_stdio(false);cin.tie(NULL);ListNode* tmp = head;if (head->val > 4) {tmp = new ListNode(1);tmp->next = head;head = tmp;tmp = tmp->next;}while (tmp->next) {tmp->val <<= 1;tmp->val %= 10;if (tmp->next->val > 4) tmp->val++;tmp = tmp->next;}tmp->val <<= 1;tmp->val %= 10;return head;}};
