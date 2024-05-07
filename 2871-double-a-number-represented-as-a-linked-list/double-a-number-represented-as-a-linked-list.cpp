72dbb8c1b8b6ba1c87f1fd107c67ce9be0ce9312
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-07 17:34:47 +0300

    Time: 163 ms (84.42%) | Memory: 120.7 MB (87.76%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -10,24 +10,4 @@
  */
 class Solution {
 public:
-    ListNode* doubleIt(ListNode* head) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        ListNode* tmp = head;
-        if (head->val > 4) {
-            tmp = new ListNode(1);
-            tmp->next = head;
-            head = tmp;
-            tmp = tmp->next;
-        }
-        while (tmp->next) {
-            tmp->val <<= 1;
-            tmp->val %= 10;
-            if (tmp->next->val > 4) tmp->val++;
-            tmp = tmp->next;
-        }
-        tmp->val <<= 1;
-        tmp->val %= 10;
-        return head;
-    }
-};
+    ListNode* doubleIt(ListNode* head) {ios_base::sync_with_stdio(false);cin.tie(NULL);ListNode* tmp = head;if (head->val > 4) {tmp = new ListNode(1);tmp->next = head;head = tmp;tmp = tmp->next;}while (tmp->next) {tmp->val <<= 1;tmp->val %= 10;if (tmp->next->val > 4) tmp->val++;tmp = tmp->next;}tmp->val <<= 1;tmp->val %= 10;return head;}};
