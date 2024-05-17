406f3bcf6e453b5b4777beb7f5abc93489459bd4
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-17 04:11:04 +0300

    Time: 17 ms (13.11%) | Memory: 21.2 MB (47.43%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -11,15 +11,14 @@
  */
 class Solution {
 public:
-    bool postorder(TreeNode* node, int target) {
-        if (node->left != nullptr) if(postorder(node->left, target)) node->left = nullptr;
-        if (node->right != nullptr) if(postorder(node->right, target)) node->right = nullptr;
+    bool postorder(TreeNode* node, int& target) {
+        if (node->left) if(postorder(node->left, target)) node->left = nullptr;
+        if (node->right) if(postorder(node->right, target)) node->right = nullptr;
         if (!node->left && !node->right) return node->val == target;
         return false;
     }
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        postorder(root, target);
-        if (!root->left && !root->right) if (root->val == target) return nullptr;
-        return root;
+        if (postorder(root, target)) return nullptr;
+        else return root;
     }
 };
