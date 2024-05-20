a4aa8424390df76a81a42094a49460d6f28567b0
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-05-20 16:31:45 +0300

    Time: 3 ms (68.97%) | Memory: 8.3 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -7,7 +7,7 @@ public:
         for (int i = 0; i < bm; ++i) {
             int x = 0;
             for (int j = 0; j < size; ++j) {
-                if (i >> j & 1) x ^= nums[j];
+                if (i & (1 << j)) x ^= nums[j];
             }
             ans += x;
         }
