18515e636eb6be66472d5f5565d061faa605ecb0
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-06-11 15:33:24 +0300

    Time: 5 ms (34.15%) | Memory: 10.2 MB (24.90%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int, int> m1;
+        vector<int> ans;
+        for (int i = 0; i < arr1.size(); ++i) {
+            m1[arr1[i]]++;
+        }
+        for (int i = 0; i < arr2.size(); ++i) {
+            int x = arr2[i];
+            while(m1[x]) {
+                m1[x]--;
+                ans.push_back(x);
+            }
+        }
+        for (auto& p : m1) {
+            if (p.second != 0) {
+                while (p.second--) {
+                    ans.push_back(p.first);
+                }
+            }
+        }
+        return ans;
+    }
+};
