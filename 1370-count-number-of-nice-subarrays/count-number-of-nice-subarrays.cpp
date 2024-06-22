c3f1bb55930023424b98aa664ff181ec93aaef87
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-06-22 22:47:05 +0300

    Time: 98 ms (47.62%) | Memory: 74.1 MB (26.18%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int l = 0, r = 1;
+        int size = nums.size();
+        int ans = 0;
+        vector<int> odds;
+        odds.push_back(-1);
+        for (int i = 0; i < size; ++i) {
+            if (nums[i] & 1) odds.push_back(i);
+        }
+        odds.push_back(size);
+        size = odds.size();
+        for (int i = 1; i < size - k; ++i) {
+            ans += (odds[i] - odds[i - 1]) * (odds[i + k] - odds[i + k - 1]);
+        }
+        return ans;
+    }
+};
