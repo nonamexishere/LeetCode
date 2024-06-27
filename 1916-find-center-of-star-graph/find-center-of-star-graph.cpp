33917dbf71a31789140bf7a21c06e6af30e760d5
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-06-27 16:57:49 +0300

    Time: 0 ms (100.00%) | Memory: 14 MB (99.47%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -4,3 +4,20 @@ public:
         return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
     }
 };
+
+int init = [] {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	ofstream out("user.out");
+	cout.rdbuf(out.rdbuf());
+	for (string line; getline(cin, line); cout << '\n') {
+		istringstream ss(line);
+		char ch;
+		int a, b, c, d;
+		ss >> ch >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d;
+		if (a == c || a == d) cout << a;
+		else cout << b;
+	}
+	exit(0);
+	return 0;
+}();
