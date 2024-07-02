3fc1a2f65b8e1ce8fa4738de65838c9e45fd0d53
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-07-02 15:36:06 +0300

    Time: 5 ms (38.58%) | Memory: 13.3 MB (41.05%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
         vector<int> ans;
-        unordered_map<int, int> m;
-        for (int& num : nums1) {
-            m[num]++;
-        }
-        for (int& num : nums2) {
-            if (m[num] > 0) {
-                m[num]--;
-                ans.push_back(num);
+        vector<int> n(1001);
+        vector<int> m(1001);
+        for (int& num : nums1) n[num]++;
+        for (int& num : nums2) m[num]++;
+        for (int i = 0; i <= 1000; ++i) {
+            int mn = min(m[i], n[i]);
+            while (mn--) {
+                ans.push_back(i);
             }
         }
         return ans;
