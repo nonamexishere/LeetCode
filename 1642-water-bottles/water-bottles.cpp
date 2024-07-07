fbe6a2d0d94dfea8602cc5db5d22575492ef0fc8
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-07-07 04:25:24 +0300

    Time: 3 ms (13.78%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         int empty = 0;
         int ans = 0;
         while (numBottles) {
