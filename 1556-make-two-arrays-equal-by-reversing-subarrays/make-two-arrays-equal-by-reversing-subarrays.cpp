0f51ea5fcd4429ce8b7eb7d76343a7fdd4193e48
nonamexishere <137460049+nonamexishere@users.noreply.github.com>
2024-08-03 16:52:19 +0300

    Time: 4 ms (92.55%) | Memory: 17.9 MB (31.70%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        ios::sync_with_stdio(false);
         vector<int> v(1001);
         for (int& i : target) {
             v[i]++;
